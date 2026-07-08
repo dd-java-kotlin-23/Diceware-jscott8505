4261a8b04166fcc937817b978f312fd1bd62d498
jazz <jscott85@student.cnm.edu>
2026-07-08 15:38:18 -0600

    Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/diceware/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive.diceware.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+  @GetMapping
+  public String get() {
+    return "Hello World!";
+  }
+
+}
